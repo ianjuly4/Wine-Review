1e31c2a1226705a96c5e928e19bd6addd79fb241
Ian July <ianjuly4@gmail.com>
2024-09-08 13:07:26 -0600

    about page update

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,9 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 function About(){
   return(
-      <h2>Whether you're just visiting, a transplant, or a Denver local looking to change things up, we got an app for you! Our Denver Restaurant Finder allows users to discover new restaurants with similar vibes and prices to their "tried and true" favorites. Cant quite remember the name of that local favorite? No problem, the search bar includes partial and complete restaurant name matches.  
-                Don't see your favorite restaurant? Don't worry, utilize the "Add" page to add in that restaurant that turned out to be a hidden gem. 
-                Not really feeling Casa Bonita anymore? Or maybe an awful experience has left a bad taste in your mouth, feel free to remove it with our delete buttons.
+      <h2>
             </h2>
   )
 }
